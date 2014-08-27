6b0c36e231dfbb718ecfd3fbe0f7a1d50ec1f8c1
Martin Volerich <martin@volerich.com>
2014-08-27 14:51:56 -0400

    Added mgo for persistance

--- main.go
+++ main.go
@@ -4,20 +4,48 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+
+	"gopkg.in/mgo.v2"
 )
+
+type EntriesHandler struct {
+	entries []Entry
+}
 
 var index = template.Must(template.ParseFiles(
 	"templates/_base.html",
 	"templates/index.html",
 ))
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	if err := index.Execute(w, nil); err != nil {
+func (e *EntriesHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+	s := session.Clone()
+	defer s.Close()
+
+	coll := s.DB("gostbook").C("entries")
+	query := coll.Find(nil).Sort("-timestamp")
+
+	if err := query.All(&e.entries); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Println(e.entries)
+
+	if err := index.Execute(w, e.entries); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
 
+var session *mgo.Session
+
 func main() {
-	http.HandleFunc("/", hello)
+	var err error
+	session, err = mgo.Dial("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Handle("/entries", &EntriesHandler{})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
